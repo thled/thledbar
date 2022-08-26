1c2fc728387a9b1c9c449bdea88fc33800bf679e
Thomas Le Duc <dev@tleduc.de>
2022-08-26 17:48:16 +0200

    Show battery charge status

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,36 @@ use std::error::Error;
 use cnx::text::*;
 use cnx::widgets::*;
 use cnx::{Cnx, Position};
+use cnx_contrib::widgets::battery::*;
 use cnx_contrib::widgets::cpu;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let attr = Attributes {
-        font: Font::new("Sauce Code Pro Nerd Font Semibold 12"),
-        fg_color: Color::white(),
-        bg_color: None,
-        padding: Padding::new(8.0, 8.0, 0.0, 0.0),
-    };
-
     let mut cnx = Cnx::new(Position::Top);
 
-    let mut active_attr = attr.clone();
-    active_attr.bg_color = Some(Color::blue());
-    cnx.add_widget(Pager::new(active_attr, attr.clone()));
+    cnx.add_widget(workspaces());
+    cnx.add_widget(window_title());
+    cnx.add_widget(cpu()?);
+    cnx.add_widget(battery());
+    cnx.add_widget(clock());
 
-    cnx.add_widget(ActiveWindowTitle::new(attr.clone()));
+    cnx.run()?;
 
+    Ok(())
+}
+
+fn workspaces() -> Pager {
+    let active_attr = Attributes {
+        bg_color: Some(Color::blue()),
+        ..default_attr()
+    };
+    Pager::new(active_attr, default_attr())
+}
+
+fn window_title() -> ActiveWindowTitle {
+    ActiveWindowTitle::new(default_attr())
+}
+
+fn cpu() -> Result<cpu::Cpu, anyhow::Error> {
     let render = Box::new(|load| {
         let mut color = Color::white().to_hex();
         if load > 10 {
@@ -31,13 +43,23 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         format!("CPU <span foreground=\"{}\">{}%</span>", color, load)
     });
-    let cpu = cpu::Cpu::new(attr.clone(), Some(render))?;
-    cnx.add_widget(cpu);
-
-    let time_template = Some("%Y-%m-%d %a %I:%M %p".into());
-    cnx.add_widget(Clock::new(attr, time_template));
-
-    cnx.run()?;
-
-    Ok(())
+    cpu::Cpu::new(default_attr(), Some(render))
 }
+
+fn battery() -> Battery {
+    Battery::new(default_attr(), Color::red(), Some("BAT1".into()), None)
+}
+
+fn clock() -> Clock {
+    Clock::new(default_attr(), Some("%Y-%m-%d %a %I:%M %p".into()))
+}
+
+fn default_attr() -> Attributes {
+    Attributes {
+        font: Font::new("Sauce Code Pro Nerd Font Semibold 12"),
+        fg_color: Color::white(),
+        bg_color: None,
+        padding: Padding::new(8.0, 8.0, 0.0, 0.0),
+    }
+}
+
